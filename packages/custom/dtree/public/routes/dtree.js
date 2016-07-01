1625a791619c2347c27003ff9bf3f0d9b9c7230d
rvishnu <vishnuvardhan.r@gmail.com>
2016-07-01 11:37:56 -0400

    Update dtree.js

--- packages/custom/dtree/public/routes/dtree.js
+++ packages/custom/dtree/public/routes/dtree.js
@@ -37,20 +37,6 @@ angular.module('mean.dtree') //, ['ngAnimate', 'ngResource', 'ui.router', 'ui.bo
             controller: 'TreeController as ctrl'
         });
 
-        $urlRouterProvider.when('/Decide2GoBack', '/decide/55a08a699f6f3594a84d324e');
-        $urlRouterProvider.when('/StartBusiness', '/decide/56525079d4c6ff06a9dfdafa');
-        $urlRouterProvider.when('/StudyMBA', '/decide/565da4cbe4b0ed465284a065');
-        $urlRouterProvider.when('/ChangeJob', '/decide/565e5523ab285b0b0047af86');
-        $urlRouterProvider.when('/GetMarried', '/decide/565e578aab285b0b0047afb5');
-
-
-
-        $urlRouterProvider.when('/ModifyDecide2GoBack', '/tree/55a08a699f6f3594a84d324e ');
-        $urlRouterProvider.when('/ModifyStartBusiness', '/tree/56525079d4c6ff06a9dfdafa');
-        $urlRouterProvider.when('/ModifyStudyMBA', '/tree/565da5bbe4b0ed465284a075');
-        $urlRouterProvider.when('/ModifyChangeJob', '/tree/565e5523ab285b0b0047af86');
-        $urlRouterProvider.when('/ModifyGetMarried', '/tree/565e578aab285b0b0047afb5');
-
- //       $viewPathProvider.override('system/views/index.html', 'dtree/views/index.html');
+       
 
     })
